3541d86d01bdcdb2edeb9fd720d10f0ce9c73828
Hackwithvyshu <64751167+keralahacker@users.noreply.github.com>
2020-08-28 01:35:41 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,49 @@
+#!/bin/bash
+clear 
+WGET="/usr/bin/wget"
+
+$WGET -q --tries=20 --timeout=10 http://www.google.com -O /tmp/google.idx &> /dev/null
+if [ ! -s /tmp/google.idx ]
+then
+	echo "                                [] "
+	echo "╔╗╔┌─┐  ╔╗╔┌─┐┌┬┐┬ ┬┌─┐┬─┐┬┌─  \||/"
+	echo "║║║│ │  ║║║├┤  │ ││││ │├┬┘├┴┐   ││"
+	echo "╝╚╝└─┘  ╝╚╝└─┘ ┴ └┴┘└─┘┴└─┴ ┴*****"
+sleep 2
+	echo "===========keralahacker==========="
+	echo ""
+	echo "This - Tool will exit in t-3sec...!"
+	echo ""
+clear
+sleep 3	
+	figlet 1
+	echo "No network conntion ples reconnect an run the script"
+sleep 2	
+clear
+	figlet 2
+	echo "No network conntion ples reconnect an run the script"
+sleep 2	
+clear	
+	figlet 3
+	echo "No network conntion ples reconnect an run the script"
+sleep 3
+clear 
+	echo "                                [] "
+	echo "╔╗╔┌─┐  ╔╗╔┌─┐┌┬┐┬ ┬┌─┐┬─┐┬┌─  \||/"
+	echo "║║║│ │  ║║║├┤  │ ││││ │├┬┘├┴┐   ││"
+	echo "╝╚╝└─┘  ╝╚╝└─┘ ┴ └┴┘└─┘┴└─┴ ┴*****"
+sleep 2	
+	echo "===========keralahacker==========="
+	echo ""
+	echo "Thank you for using this tool....!"
+	echo ""
+	echo "EXIT-ing the tool Have a nice day " | pv -qL 8
+sleep 4
+	exit
+else
+	echo "Connected..!"
+
+###############################################################
 clear 
 echo "┬┌─┌─┐┬─┐┌─┐┬  ┌─┐   ┬ ┬┌─┐┌─┐┬┌─┌─┐┬─┐"
 sleep 1
@@ -370,6 +416,6 @@ fi
 	exit
 fi
 
-
+fi
 sleep 3
 ./install.sh
